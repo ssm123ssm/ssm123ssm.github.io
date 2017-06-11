8a52ab21bc171ddbf9b830b51599925f42624865
ssm123ssm <ssm123ssm@gmail.com>
2017-06-11 12:29:31 +0530

    Df

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $(".navli").click(function(){
         $(".navli").removeClass("active");
         $(this).addClass("active");
-    });   
+    }); 
+    $(".cert").click(function(){
+        window.open("https://www.freecodecamp.com/ssm123ssm/front-end-certification", "_blank");
+    });
 });
